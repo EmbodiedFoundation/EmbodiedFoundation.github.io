b827629c099714044fe52737273fd4119d442bf9
huangshuhe <huangshu22@mails.tsinghua.edu.cn>
2025-07-17 22:41:38 +0800

    firt draft, wait for demo

--- static/js/index.js
+++ static/js/index.js
@@ -5,11 +5,11 @@ var NUM_INTERP_FRAMES = 240;
 
 var interp_images = [];
 function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
+  // for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
+  //   var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+  //   interp_images[i] = new Image();
+  //   interp_images[i].src = path;
+  // }
 }
 
 function setInterpolationImage(i) {
